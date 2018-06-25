184f09da5b82e411154886154712aaf2e61f1f29
Thomas Wilson <twilson.spqr@gmail.com>
2018-06-25 11:28:05 -0400

    constructors

--- 1through20/SphereCalcTester.java
+++ 1through20/SphereCalcTester.java
@@ -1,9 +1,9 @@
 public class SphereCalcTester {
   public static void main(String[] args) {
 
-    SphereCalc2 c = new SphereCalc2();
+    SphereCalc4 c = new SphereCalc4(5);
 
-    c.setRadius(5);
+    // c.setRadius(5);
     if (isNear(c.getSurfaceArea(), 314.159265359) ) {
       System.out.println("PASS: surfaceArea for " + c.getRadius());
     } else {
@@ -15,14 +15,15 @@ public class SphereCalcTester {
       System.out.println("FAIL volume not what was expected!");
     }
 
-    c.setRadius(0.1);
-    if (isNear(c.getSurfaceArea(), 0.125663706) ) {
-      System.out.println("PASS: surfaceArea for " + c.getRadius());
+    SphereCalc4 c2 = new SphereCalc4(0.1);
+    // c.setRadius(0.1);
+    if (isNear(c2.getSurfaceArea(), 0.125663706) ) {
+      System.out.println("PASS: surfaceArea for " + c2.getRadius());
     } else {
       System.out.println("FAIL surfaceArea not what was expected!");
     }
-    if (isNear(c.getVolume(), 4.18879E-3) ) {
-      System.out.println("PASS: volume for " + c.getRadius());
+    if (isNear(c2.getVolume(), 4.18879E-3) ) {
+      System.out.println("PASS: volume for " + c2.getRadius());
     } else {
       System.out.println("FAIL volume not what was expected!");
     }
