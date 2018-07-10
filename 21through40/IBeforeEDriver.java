ee072dfa4896149dae69e292d1fb2ab90f3940d8
Thomas Wilson <twilson.spqr@gmail.com>
2018-07-10 03:47:42 -0400

    exercises

--- 21through40/IBeforeEDriver.java
+++ 21through40/IBeforeEDriver.java
@@ -1,12 +1,29 @@
 import java.io.FileInputStream;
+import java.util.ArrayList;
 
 public class IBeforeEDriver {
   public static void main( String[] args) throws Exception {
     FileInputStream file = new FileInputStream("../datafiles/en-sample.txt");
     IBeforeEChecker check = new IBeforeEChecker(file);
+    ArrayList<String> wordList = check.getWordList();
+    int count = 0;
+
     System.out.println(check.getWordCount() + " ie/ei words found.");
     System.out.print("\t" + check.getMatches() + " match the rule, ");
     System.out.print( check.getExceptions() + " do not: ");
     System.out.println(100.0*check.getMatches()/check.getWordCount() + "%");
+
+
+
+    System.out.println("Words: ");
+    for (String word : wordList) {
+      count++;
+      if (count%3 == 0) {
+        System.out.print("\t\t" + word + "\n");
+      } else {
+        System.out.print("\t\t" + word);
+      }
+    }
+    System.out.println();
   }
 }
