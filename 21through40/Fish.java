fcab4e038ea71238353c0a081ffd90f793bc68db
Thomas Wilson <twilson.spqr@gmail.com>
2018-07-17 19:44:03 -0400

    I invented fish

---
+++ 21through40/Fish.java
@@ -0,0 +1,26 @@
+public class Fish {
+  private String name, color;
+  private String[] colors = {"red", "blue"};
+  private int id;
+  private static int count=0;
+
+  public Fish(String name) {
+    this.name = name;
+    count++;
+    id=count;
+    color=colors[(id%2)];
+    // if (id%2 == 0) {
+    //   color = "red";
+    // } else {
+    //   color = "blue";
+    // }
+  }
+
+  public String toString() {
+    return name + ", fish #" + id + " of " + count + " and is " + color;
+  }
+
+  public static int numCreated() {
+    return count;
+  }
+}
