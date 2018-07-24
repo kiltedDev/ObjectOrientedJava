fff49a28b354aeac0cdd7d55d223794e0e085bfc
Thomas Wilson <twilson.spqr@gmail.com>
2018-07-24 01:01:41 -0400

    Inheritance

---
+++ 21through40/NumberGuy.java
@@ -0,0 +1,21 @@
+public class NumberGuy {
+  private int number;
+
+  public NumberGuy() { number=0; }
+  public NumberGuy( int number ) { this.number = number; }
+  public int getNumber() { return number; }
+  public String toString() { return "" + number; }
+
+  public void doubleYourself() {
+    number = number*2;
+  }
+
+  public void changeThis( int q ) {
+    q = q*2;
+  }
+
+  public int doubleMe( int n ) {
+    int d = n*2;
+    return d;
+  }
+}
