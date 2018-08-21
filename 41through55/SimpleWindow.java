2ca11912a7267f4699b7840e373e9c5a47aefb59
Thomas Wilson <twilson.spqr@gmail.com>
2018-08-20 21:22:53 -0400

    Gui center

---
+++ 41through55/SimpleWindow.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SimpleWindow extends Application {
+  public void start(Stage stage) {
+    Group root = new Group();
+    Scene scene = new Scene(root, 600, 400);
+
+    stage.setTitle("Copabanana");
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public static void main( String[] args) {
+    launch(args);
+  }
+}
